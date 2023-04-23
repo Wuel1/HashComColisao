d8dd1dfd852f0ef9dc446e7562f0a3a84c8a3f50
Wandson Emanuel <wandson7emanuel@hotmail.com>
2023-04-23 15:22:20 -0300

    Insert Complete

--- hashColisao/EstruturaHashTable.java
+++ hashColisao/EstruturaHashTable.java
@@ -1,32 +1,73 @@
 package hashColisao;
 
-public class EstruturaHashTable implements EstruturaDeDados{
+public class EstruturaHashTable implements EstruturaDeDados {
     private Integer tabela[];
-    //TODO adicionar elementos necessários para implementar o porão
+    private Integer porao[];
 
     public EstruturaHashTable() {
-        //esse número pode ser alterado
         tabela = new Integer[1000];
+        porao = new Integer[100];
     }
 
     @Override
     public boolean insert(int chave) {
-        // TODO quando inserir, se a posição estiver ocupada, o elemento NÃO é inserido e retorna false. Caso contrário, o elemento é inserido na posição calculada e retorna true.
+        int indice = Hash(chave);
+        if (tabela[indice] == null) {
+            tabela[indice] = chave;
+            return true;
+        } else {            
+            while(indice <= porao.length){
+                if(porao[indice] == null){
+                   porao[indice] = chave;
+                   return true;
+                }
+                indice++;
+            }
+        }
         return false;
-        
     }
 
     @Override
     public boolean delete(int chave) {
-        // TODO quando inserir, se a posição estiver ocupada, torna o elemento da posição como null e retorna true. Caso contrário, retorna false.
+        int indice = Hash(chave);
+
+        // Verifica se a chave está na posição da tabela
+        if (tabela[indice] != null && tabela[indice] == chave) {
+            tabela[indice] = null;
+            return true;
+        } else {
+            // Verifica se a chave está no porão
+            for (int i = 0; i < porao.length; i++) {
+                if (porao[i] != null && porao[i] == chave) {
+                    porao[i] = null;
+                    return true;
+                }
+            }
+        }
         return false;
-        
     }
 
     @Override
     public int search(int chave) {
-        // TODO se o elemento estiver presente retorna a sua posição. Caso contrário, retorna -1.
+        int indice = Hash(chave);
+
+        // Verifica se a chave está na posição da tabela
+        if (tabela[indice] != null && tabela[indice] == chave) {
+            return indice;
+        } else {
+            // Verifica se a chave está no porão
+            for (int i = 0; i < porao.length; i++) {
+                if (porao[i] != null && porao[i] == chave) {
+                    return i + tabela.length;
+                }
+            }
+        }
         return -1;
     }
 
+    // Função de hash simples
+    private int Hash(int chave) {
+        int retornoHash = chave % tabela.length;
+        return retornoHash;
+    }
 }
